76ab18c6ab647c1de0dc839767c993c31bb0097e
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2021-10-26 12:02:30 +0530

    short changes by adding css file

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ let items = ["Buy Food", "Cook Food", "eat Food"];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 
 app.get("/", function (req, res) {
   let today = new Date();

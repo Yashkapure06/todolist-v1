b372164d973fb0c7c07d72f61b4561c5f4565631
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2021-10-26 19:01:14 +0530

    At 1st i created header.ejs and footer.ejs and called them inside list.ejs  and after that removed date code from app.js and called the date function from date.js to app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const date = require(__dirname + "/date.js");
+
 const app = express();
-let items = ["Buy Food", "Cook Food", "eat Food"];
+const items = ["Buy Food", "Cook Food", "eat Food"];
+const workItems =  [];
+
 
 app.set("view engine", "ejs");
 
@@ -11,27 +15,53 @@ app.use(express.static("public"));
 
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  
-  let options = {
-      weekday: "long",
-      day:"numeric",
-      month:"long"
-  };
-  let day = today.toLocaleDateString("en-IN", options);
- 
-  res.render("list", { kindOfDay: day, newListItems: items});
+  const day = date.getDate();
+  res.render("list", { listTitle: day, newListItems: items});
 });
 
 
 app.post("/", function (req, res) {
-    let item = req.body.newItem;
+  // console.log(req.body)
+  const item = req.body.newItem;
+  
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);   
+    res.redirect("/");
+  }
 
-    items.push(item); 
+
+    
 
     // console.log(item);
-    res.redirect("/")
+    
 })
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems })
+})
+
+app.get("/about", function (req, res) {
+  res.render("about")
+})
+
+app.post("/work", function (req, res) {
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work")
+})
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
---
+++ date.js
@@ -0,0 +1,24 @@
+
+exports.getDate = function(){
+    const today = new Date();
+      
+      const options = {
+          weekday: "long",
+          day:"numeric",
+          month:"long"
+      };
+      return today.toLocaleDateString("en-IN", options);
+     
+
+}
+exports.getDay = function(){
+    const today = new Date();
+      
+      const options = {
+          weekday: "long",
+      };
+      return today.toLocaleDateString("en-IN", options);
+      
+  
+}
+console.log(module.exports)
